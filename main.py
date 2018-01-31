8436c7d02772dda84d9968c1d231d0efcbbfb6da
khairul <khairul@gmail>
2018-01-31 03:19:04 +0000

    new file

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import requests 
+import json
+
+
+resp = requests.get('')
+
+api_token = 
+api_url_base =
+
+header = {'Content-Type':'application/json',
+          'Authorization':'A10 {0}'.format(api_token)}
+
+
