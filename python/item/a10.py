2376d4430600fe993154a25b5582e510d3607842
khairul <khairul@gmail>
2018-01-31 08:18:38 +0000

    sub to base class kwargs

--- python/item/a10.py
+++ python/item/a10.py
@@ -5,23 +5,22 @@ from machine import Machine
 
 
 class A10(Machine):
-
-	def __init__(self,username,password,ipaddr):
-		Machine.__init__(self,username,password,ipaddr)
+	def __init__(self,*args,**kwargs):
+		super(A10,self).__init__(**kwargs)
 		self.session = ''
 		self.signature = ''	
-		self.url = 'https://' + ipaddr 	
+		self.url = 'https://' + kwargs['ipaddr'] 	
 
-	def login(self):
-		header = {'Content-Type':'application/json'}
-		body = { "credentials":{"username":self.username,"password":self.password}}
-		response = requests.post( self.url+ '/axapi/v3/auth',verify=False, headers=header,json=body)
-		signature = json.loads(response.text)
-		self.signature = signature['authresponse']['signature'] 
-		print(signature)
+	#def login(self):
+		#header = {'Content-Type':'application/json'}
+		#body = { "credentials":{"username":self.username,"password":self.password}}
+		#response = requests.post( self.url+ '/axapi/v3/auth',verify=False, headers=header,json=body)
+		#signature = json.loads(response.text)
+		#self.signature = signature['authresponse']['signature'] 
+		#print(signature)
 
 #	def logoff(self):
 
-a10 = A10('admin','a10','192.168.201.34')
-a10.login()
+a10 = A10(username='admin',password='a10',ipaddr='192.168.201.34')
+#a10.login()
 
--- python/item/machine.py
+++ python/item/machine.py
@@ -4,12 +4,13 @@ import json
 import error 
 class Machine(object):
 	
-	def __init__(self,*args,**kwargs):
-		try:	
+	def __init__(self,**kwargs):
+#		try:	
 			#self.hostname = kwargs.get('hostname',"hello")	
+			print(kwargs)
 			self.username = kwargs['username']
 			self.password = kwargs['password']	
 			self.ipaddr = kwargs['ipaddr']
-		except:
-			print("Argument Error , Please check error")
+#		except:
+#	print("Argument Error , Please check error")
 
