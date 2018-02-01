240c8af5224d25f75eb57da3f7b8ec5064eead35
khairul <khairul@gmail>
2018-02-01 05:45:16 +0000

    update logoff and logon using aXAPI

--- python/item/a10.py
+++ python/item/a10.py
@@ -3,24 +3,28 @@ import requests
 import json
 from machine import Machine
 
+api_url = '/axapi/v3/'
 
 class A10(Machine):
-	def __init__(self,*args,**kwargs):
-		super(A10,self).__init__(**kwargs)
-		self.session = ''
-		self.signature = ''	
-		self.url = 'https://' + kwargs['ipaddr'] 	
+        def __init__(self,*args,**kwargs):
+            super(A10,self).__init__(**kwargs)
+            self.session = ''
+            self.signature = ''	
+            self.url = 'https://' + kwargs['ipaddr'] 	
 
-	#def login(self):
-		#header = {'Content-Type':'application/json'}
-		#body = { "credentials":{"username":self.username,"password":self.password}}
-		#response = requests.post( self.url+ '/axapi/v3/auth',verify=False, headers=header,json=body)
-		#signature = json.loads(response.text)
-		#self.signature = signature['authresponse']['signature'] 
-		#print(signature)
+        def login(self):
+            header = {'Content-Type':'application/json'}
+            body = { "credentials":{"username":self.username,"password":self.password}}
+            response = requests.post( self.url + api_url + 'auth',verify=False, headers=header,json=body)
+            signature = json.loads(response.text)
+            self.signature = 'A10 ' + signature['authresponse']['signature'] 
 
-#	def logoff(self):
+        def logoff(self):
+            header = {'Content-Type':'application/json','Authorization': self.signature}
+            response = requests.post( self.url + api_url + 'logoff',verify=False, headers=header)
+            print(json.loads(response.text))
+            
+        def ssh(self,command):
 
-a10 = A10(username='admin',password='a10',ipaddr='192.168.201.34')
-#a10.login()
-
+a10 = A10(username='admin',password='a10',ipaddr='192.168.201.15')
+print(a10.signature)
--- python/item/machine.py
+++ python/item/machine.py
@@ -2,15 +2,17 @@
 import requests
 import json
 import error 
+from netmiko import ConnectHandler
+
 class Machine(object):
-	
-	def __init__(self,**kwargs):
-#		try:	
-			#self.hostname = kwargs.get('hostname',"hello")	
-			print(kwargs)
-			self.username = kwargs['username']
-			self.password = kwargs['password']	
-			self.ipaddr = kwargs['ipaddr']
-#		except:
-#	print("Argument Error , Please check error")
+        def __init__(self,**kwargs):
+	    #try:	
+                #self.hostname = kwargs.get('hostname',"hello")	
+                print(kwargs)
+                self.username = kwargs['username']
+                self.password = kwargs['password']	
+                self.ipaddr = kwargs['ipaddr']
+            #except:
+            #    print("Argument Error , Please check error")
+        def to_ssh
 
