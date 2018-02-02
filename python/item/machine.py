ee81ae0577e2eae3124cc7d8d8edbd7c1d14c096
krollquink <krollquink@gmail.com>
2018-02-02 02:10:44 +0000

    ssh function added to class

--- python/item/machine.py
+++ python/item/machine.py
@@ -8,29 +8,30 @@ class Machine(object):
         def __init__(self,**kwargs):
 	    #try:	
                 #self.hostname = kwargs.get('hostname',"hello")	
-                print(kwargs)
                 self.username = kwargs['username']
                 self.password = kwargs['password']	
                 self.ipaddr = kwargs['ipaddr']
-                self.device_type=kwargs['device']
-            #except:
-            #    print("Argument Error , Please check error")
-        def ssh(self)
+                self.device_type = kwargs['device_type']
                 machine = {
-                        'device-type':self.device_type,
+                        'device_type':self.device_type,
                         'username':self.username,
                         'password':self.password,
                         'ip':self.ipaddr
                         }
                 self.ssh = ConnectHandler(**machine)
+            #except:
+            #    print("Argument Error , Please check error")
                
-        def logoff_ssh(self)
+        def logoff_ssh(self):
                 self.ssh.disconnect()
 
+##For testing Class##
+#test = Machine(username='admin',password='a10',ipaddr='192.168.201.15',device_type='a10')
+#
+#output = test.ssh.send_command("show int brief");
+#print(output)        
+#
+#test.ssh.logoff_ssh()
 
-test = Machine(username='admin',password='a10',ipaddr='192.168.201.15',device='a10')
-
-test.ssh.send_command("show int brief");
-                
 
 
