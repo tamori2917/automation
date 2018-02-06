04ff9a1f4faf057a803a5eded3b5f744f3eb730e
krollquink <krollquink@gmail.com>
2018-02-06 05:40:45 +0000

    testing conflict

--- python/item/a10.py
+++ python/item/a10.py
@@ -27,4 +27,5 @@ class A10(Machine):
         def ssh(self,command):
 
 a10 = A10(username='admin',password='a10',ipaddr='192.168.201.15')
-print(a10.signature)
+print(a10.signature).my best
+this is the latest please try
