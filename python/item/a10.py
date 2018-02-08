36b51fa8f1932f8f51d47dd9ecc40687a24bef73
mori <mori@example.com>
2018-02-08 17:02:09 +0900

    17221_0208

--- python/item/a10.py
+++ python/item/a10.py
@@ -1,24 +1,15 @@
 
 import requests 
 import json
+import json_replace
 from machine import Machine
 
 api_url = '/axapi/v3/'
 null = 'null'
 
-##import pyload json  as dictionary
-f = open('class-list1.json', 'r')
-f1 = open ('access-list-exd.json', 'r')
-##change as string
-Allf = f.read()
-Allf1 = f1.read()
-##delete new line for linux and mac and windows
-text = Allf.replace('\n','')
-jsonData = text.replace('\r','')
-
-text1 = Allf1.replace('\n','')
-jsonData1 = text1.replace('\r','')
-
+##reform json_data
+jsonData = json_replace.Json_replace("class-list1.json")
+jsonData1 = json_replace.Json_replace("access-list-exd.json")
 class A10(Machine):
 	def __init__(self,*args,**kwargs):
 		super(A10,self).__init__(**kwargs)
@@ -27,6 +18,7 @@ class A10(Machine):
 		self.url = 'https://' + kwargs['ipaddr'] 	
 		self.header = {'Content-Type':'application/json'}
 		self.payload = jsonData
+		self.payload1 = jsonData1
 		print (self.payload)
 
 	def login(self):
@@ -62,8 +54,8 @@ class A10(Machine):
 		header = {'Content-Type':'application/json','Authorization': self.signature}
 		if method == "get":
 			response = requests.get( self.url + api_url + "ip/" + "access-list/" + "standard" + "99", verify=False, headers=header )
-#		elif method == "post":
-#			response = requests.get( self.url + api_url + "access-list", verify=False, headers=header )
+		elif method == "post":
+			response = requests.post( self.url + api_url + "access-list", verify=False, headers=header, data=self.payload1)
 #		elif method == "put":
 #			response = requests.get( self.url + api_url + "access-list", verify=False, headers=header )
 		else:
@@ -73,5 +65,5 @@ class A10(Machine):
 a10 = A10(username='admin',password='a10',ipaddr='192.168.201.31' ,device_type='a10')
 a10.login()
 #a10.classlist("put")
-a10.accesslist("get")
+a10.accesslist("post")
 a10.logoff()
