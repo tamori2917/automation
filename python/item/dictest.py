cce74adc2e0048399d67bfe5faa998c2c93df7eb
krollquink <krollquink@gmail.com>
2018-02-13 04:35:35 +0000

    clean all

--- python/item/dictest.py
+++
@@ -1,8 +0,0 @@
-
-
-#header = 
-
-header = {'Content-Type':'application/json'}
-#header_new = header.update({'Authorization': 'hello'})
-header['Authorization'] = 'hello'
-print(header)
--- python/item/python/item/error.py
+++
@@ -1,6 +0,0 @@
-
-
-def checkError(missArg,dict):
-	if 'errormessage' in kwargs:
-		print('{0} is missing Please define {0} '.format(missArg))
-
